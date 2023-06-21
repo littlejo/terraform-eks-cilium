7fbede8bff02cc67f550548dec227f3ad8738c21
Joseph Ligier <joseph.ligier@accenture.com>
2023-06-21 12:56:13 +0200

    Fix tflint

--- versions.tf
+++ versions.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "> 5.0"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "> 2.0"
+    }
   }
   required_version = "> 1.4"
 }
